b80edc536e79f4db3ee32096f19d3d7ba969026e
Guilherme <gsilva12401321@gmail.com>
2026-03-11 21:53:24 -0300

    Foi iniciada a construção da função de criar o script ao clicar no botão

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// Algoritmo Receita do Bolo
+// Lógica de Programação - Fazer o bolo
+
+// Algoritmo
+
+// [x] Saber quem é o botão
+// [ ] Saber quando o botão foi clicado
+// [ ] Saber quem é o textarea
+// [ ] Pegar o que tem dentro dele
+// [ ] Enviar para a IA
+// [ ] Pegar a resposta da IA e colocar na tela
+
+let botao = document.getElementById('botao')
+
+botao.addEventListener('click', function() {})
