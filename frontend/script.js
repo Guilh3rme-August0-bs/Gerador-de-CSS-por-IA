88963d38124ffcb70f29958c02c3b4f9d5e67bb6
Guilherme <gsilva12401321@gmail.com>
2026-03-19 20:50:10 -0300

    Adaptações para hospedar o site oficialmente

--- frontend/script.js
+++ frontend/script.js
@@ -1,37 +1,56 @@
-let blocoCodigo = document.querySelector('.bloco-codigo')
-let resultadoCodigo = document.querySelector('.resultado-codigo')
+// Elementos da interface
+let blocoCodigo = document.querySelector('.bloco-codigo') // onde o código será exibido
+let resultadoCodigo = document.querySelector('.resultado-codigo') // iframe onde o código será renderizado
 
 let botao = document.getElementById('botao');
 let prompt_box = document.getElementById('prompt-box');
-//let chave = '' //chave desativada por segurança
-//let endereco = "https://api.groq.com/openai/v1/chat/completions"
-let endereco = "http://localhost:3000/generate"
+
+// URL do backend (agora você não chama mais a API direto)
+let endereco = "https://gerador-de-css-por-ia.onrender.com/"
+//"http://localhost:3000/generate"
 
 async function gerarCodigo() {
 
+    // Requisição para o SEU backend (não mais para a API externa)
     let resposta = await fetch(endereco, {
+
         method: "POST",
+
         headers: {
-            "Content-Type": "application/json",
-            //"Authorization": "Bearer " + chave
+            "Content-Type": "application/json"
         },
+
+        // Envia o prompt digitado pelo usuário
         body: JSON.stringify({
             prompt: prompt_box.value
         })
     })
+
+    // Converte resposta do backend
     let dados = await resposta.json();
+
+    // Recebe o resultado já processado pelo backend
     let resultado = dados.resultado;
 
+    // Exibe o código como texto
     blocoCodigo.textContent = resultado
-    resultadoCodigo.srcdoc = resultado
-    
 
+    // Renderiza o HTML dentro do iframe
+    resultadoCodigo.srcdoc = resultado
+
+    //Erro caso o código não puder ser gerado
+    if (!resposta.ok) {
+    alert("Erro ao gerar código")
+    return
+}
 }
 
+// Evento de clique no botão
 botao.addEventListener('click', function () {
 
-    if (prompt_box.value === "") { 
-alert("Prompt Vazio!")
+    // Validação simples para evitar requisição vazia
+    if (prompt_box.value === "") {
+        alert("Prompt Vazio!")
     } else {
         gerarCodigo()
     }
